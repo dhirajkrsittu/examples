9ea0532668007adb166060f0be6eff9a84374e23
dhirajkrsittu <djksittu@gmail.com>
2025-10-16 12:50:46 +0530

    Add XML parsing for T52 and P44A in Groovy script

--- check_xml_t52_p44a.groovy
+++ check_xml_t52_p44a.groovy
@@ -72,3 +72,49 @@ if (contentText == null || contentText == "") {
 } else {
     println(contentText)
 }
+
+
+
+
+///////////////////CPI groovy below from system////////////
+import com.sap.gateway.ip.core.customdev.util.Message;
+import groovy.util.XmlSlurper;
+
+def Message processData(Message message) {
+    //Body
+    def xmlData = message.getBody(java.lang.String)
+    def ODataErrorBody = ""
+    
+    def root = new XmlSlurper().parseText(xmlData)
+    
+    //it checks the first occurence of T52 in the xml and returns the docRefNode for it.
+    def docRefNode = root.TransportationOrder.BusinessTransactionDocumentReference.BusinessTransactionDocumentReference.find {
+    "T52".equalsIgnoreCase(it.TypeCode.text())
+    }
+    
+    def id = docRefNode ? docRefNode.ID.text() : null
+    
+    if (id == null || id == "") {
+        //message.setProperty("ODataErrorBody1","T52 not found in BusinessTransactionDocumentReference");
+        ODataErrorBody = ODataErrorBody + "T52 not found in BusinessTransactionDocumentReference. "
+    } else {
+        message.setProperty("id_t52",id)
+    }
+    
+    def textNode = root.TransportationOrder.TextCollection.Text.find {
+        "p44a".equalsIgnoreCase(it.TypeCode.text())
+    }
+    
+    def contentText = textNode ? textNode.ContentText.text() : null
+    
+    if (contentText == null || contentText == "") {
+        //message.setProperty("ODataErrorBody2","P44A not found in TextCollection");
+        ODataErrorBody = ODataErrorBody + "P44A not found in TextCollection."
+    } else {
+        message.setProperty("contentText_p44a",contentText)
+    }
+    
+    message.setProperty("ODataErrorBody",ODataErrorBody)
+    
+    return message
+}
